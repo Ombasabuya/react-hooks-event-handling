cae0e884512452230a008aeacd02acf679c6e868
ombasaabuya <ombasabuya@gmail.com>
2023-01-18 08:24:26 +0300

    completed

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+// function Tickler() {
+//   function tickle() {
+//     console.log("Teehee!");
+//   }
+
+//   return <button onClick={tickle}>Tickle me!</button>;
+// }
 function Tickler() {
-  function tickle() {
-    console.log("Teehee!");
-  }
-
-  return <button>Tickle me!</button>;
+  return <button onClick={() => console.log("Teehee!")}>Tickle me!</button>;
 }
-
 export default Tickler;
